142bfc2efb608f02ce844e683c19520e8c38c094
Rodney Pantonial <rodney.pantonial@gmail.com>
2019-01-15 11:03:40 +0800

    Added initial code for angular builder.

---
+++ src/environment.builder.ts
@@ -0,0 +1,12 @@
+import { Builder, BuilderConfiguration, BuilderContext, BuildEvent } from '@angular-devkit/architect';
+import { Observable, of } from 'rxjs';
+import { IEnvironmentSchema } from './schema';
+
+export default class TimestampBuilder implements Builder<IEnvironmentSchema> {
+    constructor(private context: BuilderContext) {
+    }
+
+    run(builderConfig: BuilderConfiguration<Partial<IEnvironmentSchema>>): Observable<BuildEvent> {
+        return of({ success: true });
+    }
+}
