a25803307517498c334343410501fc021fecc895
Rodney Pantonial <rodney.pantonial@gmail.com>
2019-01-16 23:50:01 +0800

    v0.0.11

--- src/environment.builder.ts
+++ src/environment.builder.ts
@@ -42,8 +42,7 @@ export default class EnvironmentBuilder implements Builder<IEnvironmentSchema> {
     };
 
     // noinspection JSUnusedGlobalSymbols
-    constructor(private context: BuilderContext) {
-    }
+    constructor(private context: BuilderContext) {}
 
     public run(builderConfig: BuilderConfiguration<Partial<IEnvironmentSchema>>): Observable<BuildEvent> {
         const root = this.context.workspace.root;
@@ -57,7 +56,7 @@ export default class EnvironmentBuilder implements Builder<IEnvironmentSchema> {
         const environment = EnvironmentBuilder.load(srcModule);
 
         const options = {
-            quote: '\'',
+            quote: "'",
             space: 4,
         };
         const outputJson = json5.stringify(environment, options);
