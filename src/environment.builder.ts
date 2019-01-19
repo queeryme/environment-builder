e2ecf1635bacd13fc312e2ced848e3b6286470b1
Rodney Pantonial <rodney.pantonial@gmail.com>
2019-01-19 11:04:29 +0800

    Added documentation and moved code to functions for readability.

--- src/environment.builder.ts
+++ src/environment.builder.ts
@@ -4,14 +4,22 @@ import * as ejs from 'ejs';
 import { writeFile } from 'fs';
 import * as json5 from 'json5';
 import * as path from 'path';
-import { bindNodeCallback, from, Observable, of } from 'rxjs';
+import { bindNodeCallback, from, Observable, of, throwError } from 'rxjs';
 import { catchError, flatMap, map } from 'rxjs/operators';
 import * as ts from 'typescript';
 import { CompilerOptions, Diagnostic, ModuleKind, ModuleResolutionKind, ScriptTarget } from 'typescript';
 import { IEnvironmentModule, IEnvironmentSchema } from './schema';
 
+export const DEFAULT_TEMPLATE = 'environment.ts.ejs';
+
 // noinspection JSUnusedGlobalSymbols
 export default class EnvironmentBuilder implements Builder<IEnvironmentSchema> {
+    /**
+     * Loads the environment source file by requiring it then returning the corresponding environment.
+     * This will give you a full access to the NodeJS and is a potential security risk.
+     * Only run trusted scripts here.
+     * @param srcModule
+     */
     private static load(srcModule: string): object {
         let srcRequired: IEnvironmentModule;
         try {
@@ -25,7 +33,22 @@ export default class EnvironmentBuilder implements Builder<IEnvironmentSchema> {
         return srcRequired.environment;
     }
 
-    // TODO: make compilerOptions configurable
+    private static render(environment: object, model?: string, modelPath?: string, template?: string): Promise<string> {
+        // TODO: make json5 options configurable
+        const options = {
+            quote: '\'',
+            space: 4,
+        };
+        const outputJson = json5.stringify(environment, options);
+        const data = { model, modelPath, environment: outputJson };
+        let templateToUse = template;
+        if (!templateToUse) {
+            templateToUse = path.join(__dirname, DEFAULT_TEMPLATE);
+        }
+        return ejs.renderFile<string>(templateToUse, data);
+    }
+
+    // TODO: make compilerOptions configurable and move defaults to a json file to be read for cleanliness
     private readonly compilerOptions: CompilerOptions = {
         moduleResolution: ModuleResolutionKind.NodeJs,
         module: ModuleKind.CommonJS,
@@ -45,11 +68,14 @@ export default class EnvironmentBuilder implements Builder<IEnvironmentSchema> {
     };
 
     // noinspection JSUnusedGlobalSymbols
-    constructor(private context: BuilderContext) {}
+    constructor(private context: BuilderContext) {
+    }
 
     public run(builderConfig: BuilderConfiguration<Partial<IEnvironmentSchema>>): Observable<BuildEvent> {
         const root = this.context.workspace.root;
         const { src, dotenvConfigOptions, model, modelPath, template, output } = builderConfig.options;
+
+        // This allows a .dotenv file to configure the environment
         require('dotenv').config(dotenvConfigOptions);
 
         const srcFile = `${getSystemPath(root)}/${src}.ts`;
@@ -58,33 +84,25 @@ export default class EnvironmentBuilder implements Builder<IEnvironmentSchema> {
         const srcModule = `${getSystemPath(root)}/${src}`;
         const environment = EnvironmentBuilder.load(srcModule);
 
-        const options = {
-            quote: "'",
-            space: 4,
-        };
-        const outputJson = json5.stringify(environment, options);
-        const data = { model, modelPath, environment: outputJson };
-        let templateToUse = template;
-        if (!templateToUse) {
-            templateToUse = path.join(__dirname, 'environment.ts.ejs');
-        }
-        const renderObservable = from(ejs.renderFile(templateToUse, data));
+        const renderPromise = EnvironmentBuilder.render(environment, model, modelPath, template);
+        const renderObservable = from<string>(renderPromise);
+
         return renderObservable.pipe(
             flatMap((renderedString, error) => {
                 if (error || !output) {
-                    return of({ success: false });
+                    return throwError('Invalid Input File');
                 }
                 const outputFile = `${getSystemPath(root)}/${output}`;
-                const writeFileObservable = bindNodeCallback(writeFile);
-                return writeFileObservable(outputFile, renderedString).pipe(
-                    map(() => ({ success: true })),
-                    catchError(() => of({ success: false })),
-                );
+                const writeFileObservableBuilder = bindNodeCallback(writeFile);
+                return writeFileObservableBuilder(outputFile, renderedString);
             }),
+            map(() => ({ success: true })),
+            catchError(() => of({ success: false })),
         );
     }
 
     /**
+     * Compiles a list of typescript files into its javascript equivalent.
      * @param fileNames - a list of files that ends with a `ts` to compile
      * @param options - Options used to compile the ts files
      * @see CompilerOptions
